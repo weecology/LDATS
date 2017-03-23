24d87ebd36afd92701de988ef82c40a363b7a485
Erica Christensen <erica.christensen@weecology.org>
2017-03-23 15:34:28 -0400

    added WAIC calculation -- work in progress

--- AIC_model_selection.R
+++ AIC_model_selection.R
@@ -43,3 +43,26 @@ aic_model_gibbs = function(dat,nspp,tsteps) {
   }
   return(aic_values)
 }
+
+
+# WAIC -- doesn't work yet
+
+waic_model_gibbs = function(lda) {
+  ll = purrr::map_dbl(lda@fitted, function(x) x@loglikelihood)
+  # "Effective sample size:" should probably be at least 1000.
+  # Should also run multiple MCMC chains and evaluate coda::gelman.diag
+  coda::effectiveSize(ll)
+  # Numerically stable way to compute the mean likelihood (as opposed to mean
+  # _log_-likelihood)
+  log_mean_exp = function(x) {
+    m = max(x)
+    x = x - m
+    log(mean(exp(x))) + m
+  }
+  # WAIC (specifically, WAIC2 as defined by Equations 11 & 13 in
+  # http://www.stat.columbia.edu/~gelman/research/published/waic_understand3.pdf)
+  lppd = log_mean_exp(ll)
+  p_waic2 = var(ll)
+  -2 * (lppd - p_waic2)
+  
+}
