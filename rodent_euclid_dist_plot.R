89b82c6127d627b54501ba812867698528aac68e
Erica Christensen <erica.christensen@weecology.org>
2017-06-01 15:12:25 -0400

    add script for making euclidean distance boxplot (Figure 1)

---
+++ rodent_euclid_dist_plot.R
@@ -0,0 +1,48 @@
+#Script for making the euclidean distance boxplot (Figure 1)
+library(vegan)
+library(dplyr)
+library(ggplot2)
+
+# read in latest Rodent data, make a table of species/time
+rdat = read.csv('C:/Users/EC/Desktop/git/PortalData/Rodents/Portal_rodent.csv',stringsAsFactors = F,na.strings = '')
+granivores = c('BA','DM','DO','DS','PB','PH','PI','PP','PF','PE','PM','RM','RO','RF')
+
+# read in trapping data and find censuses where all control plots were trapped
+trapdat = read.csv('C:/Users/EC/Desktop/git/PortalData/Rodents/Portal_rodent_trapping.csv')
+allplots = aggregate(trapdat$Sampled,by=list(period=trapdat$Period),FUN=sum) %>% filter(x>21)
+
+# create table of species abundances
+absabund = rdat %>% filter(period>0, period< 437, period %in% allplots$period, species %in% granivores, plot %in% c(2,4,8,11,12,14,17,22)) %>% select(period,species) %>% table()
+
+# load trapping date data
+pdat = read.csv('C:/Users/EC/Desktop/git/PortalData/Rodents/moon_dates.csv',stringsAsFactors = F)
+pdat$CensusDate = as.Date(pdat$CensusDate)
+
+# set up for yearly aggregation
+absdat_dates = merge(absabund,pdat,by.x='period',by.y='Period')
+absdat_dates$year = format(absdat_dates$CensusDate,'%Y')
+
+absdat_yr = aggregate(absdat_dates$Freq,by=list(year = absdat_dates$year,species=absdat_dates$species),FUN=mean)
+
+yearly_table = data.frame(reshape(absdat_yr,timevar='species',idvar='year',direction='wide'))
+yearly_avg_total = data.frame(year = as.numeric(yearly_table$year),total = rowSums(yearly_table[,-1]))
+
+relative_yr = yearly_table[,-1]/yearly_avg_total$total
+# adjacent pairs
+ED_yr = as.matrix(vegdist(relative_yr,method='euclidean'))
+
+
+#====================
+# all the lag times (Thibault 2004) (only used the 14 granivores)
+bylag = c()
+for (n in 1:37) {
+  bylag = rbind(bylag,cbind(diag(ED_yr[,-(1:n)]),rep(n)))
+}
+
+df = data.frame(bylag)
+ggplot(data.frame(bylag),aes(x=X2,y=X1,group=X2)) +
+  geom_boxplot() +
+  scale_y_continuous(name='Euclidean Distance') +
+  scale_x_continuous(name='Lag Years')
+
+#boxplot(bylag[,1]~bylag[,2],xlab='lag years',ylab='Euclidean dist', main='by lag time')
