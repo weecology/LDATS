a93281c74ed0ad64b594ff5f762468765f651364
DAPPERstats <dapperstats@gmail.com>
2018-04-06 00:35:59 -0700

    tidying of the package space
    removed all non-needed packages from DESCRIPTION file
    included must-have packages as Depends (for special operations: magrittr, doParallel, and doRNG
    included the rodent data as needed as a data file and removed all the data processing functions (moved them to previous work subfolder)

--- R/classes.R
+++ R/classes.R
@@ -1,5 +1,5 @@
 setMethod("plot", signature(x = "LDA", y = "ANY"), 
-  function(x, y, cols) {
-    plot_LDA(x, cols)
+  function(x, y) {
+    plot_LDA(x, cols = NULL)
   }
 )
--- package_dev.R
+++ package_dev.R
@@ -1,13 +1,3 @@
-pkg_depends <- c("topicmodels", "RCurl", "multipanelfigure", "reshape2",  
-                 "dplyr", "memoise", "lubridate", "progress", "ggplot2",  
-                 "viridis", "nnet", "RColorBrewer", "Rcpp", "bindrcpp", 
-                 "tidyverse", "gridExtra", "topicmodels", "doParallel",
-                 "doRNG", "rlang")
-n_pkgs <- length(pkg_depends)
-for(i in 1:n_pkgs){
-  devtools::use_package(pkg_depends[i], "Imports")
-}
-
 devtools::load_all()
 devtools::document()
 #devtools::build()
