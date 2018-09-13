9fd18decb57689030c6e0adf2e4d6e9f8808f523
DAPPERstats <dapperstats@gmail.com>
2018-09-13 00:48:57 -0700

    pull that

--- working/devspace.R
+++ working/devspace.R
@@ -2,6 +2,7 @@ devtools::load_all()
 data(rodents)
 rem <- which(colnames(rodents) %in% c("newmoon", "date", "plots", "traps"))
 lda_data <- rodents[,-rem]
+dct <- data.frame(newmoon = rodents[,"newmoon"])
 
 r_lda <- LDA_set(MV = lda_data, topics = 2, nseeds = 2)
 smod <- select_LDA(r_lda)
@@ -10,3 +11,6 @@ slotNames(x1)
 
 yy <- smod[[1]]@gamma
 XX(yy~1)
+
+
+TS_set_on_LDA(r_lda, dct, "newmoon")
