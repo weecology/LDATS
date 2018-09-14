e4397dad702b095ceefc567f165677e56e87bda4
DAPPERstats <dapperstats@gmail.com>
2018-09-13 19:37:01 -0700

    put TS_on_LDA in its own script and use TS script for TS

--- working/devspace.R
+++ working/devspace.R
@@ -4,7 +4,7 @@ rem <- which(colnames(rodents) %in% c("newmoon", "date", "plots", "traps"))
 lda_data <- rodents[,-rem]
 dct <- data.frame(newmoon = rodents[,"newmoon"])
 
-r_lda <- LDA_set(MV = lda_data, topics = 2, nseeds = 2)
+r_lda <- LDA_set(lda_data, topics = 2, nseeds = 2)
 smod <- select_LDA(r_lda)
 
 slotNames(x1)
