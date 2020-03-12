7b93bf6c7f1964f758e97797b50640aaccf1a822
DAPPERstats <dapperstats@gmail.com>
2020-03-12 12:39:23 -0700

    Update testing_example.R

--- testing_example.R
+++ testing_example.R
@@ -1,9 +1,15 @@
 devtools::load_all()
 data(rodents)
 length(rodents)
+
 rodents[["metadata"]] <- list(timename = "newmoon")
+names(rodents) <- c("abundance", "covariates", "metadata")
 
 rodents[["metadata"]] <- NULL
+names(rodents)[1] <- "term"
 mod <- LDA(rodents)
 mod <- LDA(rodents, topics = 1)
 mod <- LDA(rodents, control = LDA_control(LDA_args = list(method = "Gibbs")))
+
+mod <- LDA(rodents[[1]])
+
