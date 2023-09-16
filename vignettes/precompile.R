9bf2f1d79805a7fd682cc4fb37fec95d03053d2e
juniperlsimonis <dapperstats@gmail.com>
2023-09-15 19:29:00 -0700

    precompile and relocate figures

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,6 +1,13 @@
-#Vignettes that depend on internet access have been precompiled:
-
+# Vignettes that depend on internet access have been precompiled:
 
 knitr::knit("vignettes/paper-comparison.Rmd.remote_needs", output = "vignettes/paper-comparison.Rmd")
+
+# Manually move figure files from /figure to /vignettes/figure
+
+dir.create(file.path("vignettes", "figure"), showWarnings = FALSE)
+file.copy(file.path("figure"), file.path("vignettes"), recursive = TRUE)
+unlink(file.path("figure"), force = TRUE, recursive = TRUE)
+
+# Build checking
 
 devtools::build_vignettes( )
